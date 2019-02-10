beb53c1185b21c159d20d78d02bc6b4e0a6acb3a
blaka90 <blaka90@hotmail.co.uk>
2019-02-10 00:09:36 +0000

    added config files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='sync_er',
+	version='1.6',
+	packages=['netifaces', "pyqt5"],
+	url='',
+	license='',
+	author='blaka7',
+	author_email='',
+	description=''
+)
