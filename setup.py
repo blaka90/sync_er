850ac836634c2592b466f9b83c0ebecdc9150045
blaka90 <blaka90@hotmail.co.uk>
2019-03-07 18:49:58 +0000

    updated

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
 	name='sync_er',
-	version='1.6',
-	packages=['netifaces', "pyqt5", "scp", "paramiko"],
+	version='1.7.2',
+	packages=['netifaces', "pyqt5", "scp", "paramiko", "pexpect"],
 	url='',
-	license='',
+	license='GPLv3',
 	author='blaka7',
-	author_email='',
+	author_email='blaka90@hotmail.co.uk',
 	description='Cross Platform Gui wrapper for rsync and scp'
 )
