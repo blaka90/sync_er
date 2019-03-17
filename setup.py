82febe5b93b1719909365e772950e0c478a3218b
blaka90 <blaka90@hotmail.co.uk>
2019-03-17 20:45:50 +0000

    reverted version numbers to pre-release numbers(hindsight they should have been from start)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='sync_er',
-	version='1.7.2',
+	version='0.7.5',
 	packages=['netifaces', "pyqt5", "scp", "paramiko", "pexpect"],
 	url='',
 	license='GPLv3',
