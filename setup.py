184cb510a8db6dd776d9d747871247ef8ceaf772
blaka90 <blaka90@hotmail.co.uk>
2021-08-15 20:58:32 +0100

    version bump 0.8.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='sync_er',
-	version='0.8.5',
+	version='0.8.6',
 	packages=['netifaces', "pyqt5", "scp", "paramiko", "pexpect"],
 	url='',
 	license='GPLv3',
